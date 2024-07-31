15f76faba344e29e89f5cc6a7106a9c62116379a
yatriidungarani <dungaraniyatri@gmail.com>
2024-07-31 13:25:16 +0530

    Create practical9.java

---
+++ part2/practical9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class practical9{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		char x;
+		for(int i=0;i<str.length();i++){
+			x=str.charAt(i);
+			for(int j=0;j<2;j++){
+				System.out.print(x);
+			}
+			
+		}
+	    
+	}
+}
