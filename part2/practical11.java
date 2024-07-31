945efaa09f89fb552d0a59e47a0b1de6c012339a
yatriidungarani <dungaraniyatri@gmail.com>
2024-07-31 13:26:30 +0530

    Create practical11.java

---
+++ part2/practical11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class practical11{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		System.out.println("Length:"+str.length());
+		String str1=str.replace('H','Y');
+		System.out.println(str1);
+		System.out.println(str1.toLowerCase());
+	    
+	}
+}
