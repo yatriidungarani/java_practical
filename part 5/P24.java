00a4a77ddf5fdae91b20ca22056c508b0e5f695d
yatriidungarani <dungaraniyatri@gmail.com>
2024-10-19 11:54:51 +0530

    Create P24.java

---
+++ part 5/P24.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class P24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            
+            System.out.print("Enter the first integer (x): ");
+            int x = scanner.nextInt();
+            System.out.print("Enter the second integer (y): ");
+            int y = scanner.nextInt();
+
+            
+            int result = x / y;
+            System.out.println("The result of x / y is: " + result);
+
+        } 
+catch (ArithmeticException e) {
+          
+            System.out.println("Division by zero is not allowed.");
+
+        } finally {
+            
+            scanner.close();
+        }
+    }
+    
+}
