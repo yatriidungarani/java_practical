4d64210c9b7b1df6416339898dd9f7bc18861fb3
yatriidungarani <dungaraniyatri@gmail.com>
2024-10-19 12:03:57 +0530

    Create P35.java

---
+++ part 7/P35.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+public class P35 implements Runnable {
+  public static void main(String args[]){
+  P35 t1=new P35();
+  Thread t=new Thread(t1);
+  t.start();
+  }
+
+@Override
+public void run() {
+     for(int i=0;i<10;i++){
+        System.out.println(i);
+        try {
+            Thread.sleep(1000); // Sleep for 2 seconds
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted.");
+        }     } 
+} 
+}
