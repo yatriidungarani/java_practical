d7356aee8034433971ea2df1335e248cbe84a8d5
yatriidungarani <dungaraniyatri@gmail.com>
2024-10-19 11:48:46 +0530

    Create P17.java

---
+++ part 4/P17.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class P17{
+     P17(){
+        System.out.println("This is parent class");
+    }
+    public static void main(String args[]){
+
+        child c=new child();
+        c.dis();
+       
+    }
+}
+class child extends P17{
+    void dis(){
+    System.out.println("This is child class.");
+}
+}
